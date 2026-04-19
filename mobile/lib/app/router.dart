cf712944ff69170dc935e1951758947038e82950
TgkCapture <tawongakanyenda5@gmail.com>
2026-04-19 10:17:30 +0200

    fix: clear warnings

--- mobile/lib/app/router.dart
+++ mobile/lib/app/router.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:openair/features/home/screens/home_screen.dart';
 import 'package:openair/features/tv/screens/tv_screen.dart';
@@ -13,16 +13,16 @@ class AppRouter {
   static final router = GoRouter(
     initialLocation: '/home',
     routes: [
-      GoRoute(path: '/login', builder: (_, __) => const LoginScreen()),
-      GoRoute(path: '/register', builder: (_, __) => const RegisterScreen()),
+      GoRoute(path: '/login', builder: (_, _) => const LoginScreen()),
+      GoRoute(path: '/register', builder: (_, _) => const RegisterScreen()),
       ShellRoute(
         builder: (context, state, child) => AppShell(child: child),
         routes: [
-          GoRoute(path: '/home', builder: (_, __) => const HomeScreen()),
-          GoRoute(path: '/tv', builder: (_, __) => const TvScreen()),
-          GoRoute(path: '/radio', builder: (_, __) => const RadioScreen()),
-          GoRoute(path: '/library', builder: (_, __) => const LibraryScreen()),
-          GoRoute(path: '/profile', builder: (_, __) => const ProfileScreen()),
+          GoRoute(path: '/home', builder: (_, _) => const HomeScreen()),
+          GoRoute(path: '/tv', builder: (_, _) => const TvScreen()),
+          GoRoute(path: '/radio', builder: (_, _) => const RadioScreen()),
+          GoRoute(path: '/library', builder: (_, _) => const LibraryScreen()),
+          GoRoute(path: '/profile', builder: (_, _) => const ProfileScreen()),
         ],
       ),
     ],
