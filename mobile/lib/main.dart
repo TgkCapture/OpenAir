c37be18c27aff945da3c2c3b53ca9d11e3101dc6
TgkCapture <tawongakanyenda5@gmail.com>
2026-04-21 16:08:12 +0200

    feat: add media kit

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:media_kit/media_kit.dart';
 import 'package:openair/app/app.dart';
 import 'package:openair/core/di/injection.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  MediaKit.ensureInitialized();
   configureDependencies();
   runApp(const OpenAirApp());
 }
