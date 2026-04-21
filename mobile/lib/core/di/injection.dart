c958a6df0fdc29056bee106e11b3545f80582675
TgkCapture <tawongakanyenda5@gmail.com>
2026-04-21 11:45:23 +0200

    feat: ApiService → AuthRepository → AuthBloc Closes #13, closes #14

--- mobile/lib/core/di/injection.dart
+++ mobile/lib/core/di/injection.dart
@@ -1,8 +1,14 @@
 import 'package:get_it/get_it.dart';
+import 'package:openair/core/services/api_service.dart';
+import 'package:openair/features/auth/bloc/auth_bloc.dart';
+import 'package:openair/features/auth/repository/auth_repository.dart';
 import 'package:openair/features/settings/bloc/settings_bloc.dart';
 
 final getIt = GetIt.instance;
 
 void configureDependencies() {
-  getIt.registerFactory(() => SettingsBloc());
+  getIt.registerLazySingleton<ApiService>(() => ApiService());
+  getIt.registerLazySingleton<AuthRepository>(() => AuthRepository(getIt<ApiService>()));
+  getIt.registerFactory<AuthBloc>(() => AuthBloc(getIt<AuthRepository>()));
+  getIt.registerFactory<SettingsBloc>(() => SettingsBloc());
 }
---
+++ mobile/lib/features/auth/bloc/auth_event.dart
@@ -0,0 +1,23 @@
+part of 'auth_bloc.dart';
+
+abstract class AuthEvent extends Equatable {
+  const AuthEvent();
+  @override List<Object> get props => [];
+}
+
+class AuthLoginRequested extends AuthEvent {
+  final String email;
+  final String password;
+  const AuthLoginRequested(this.email, this.password);
+  @override List<Object> get props => [email, password];
+}
+
+class AuthRegisterRequested extends AuthEvent {
+  final String email;
+  final String password;
+  final String fullName;
+  const AuthRegisterRequested(this.email, this.password, this.fullName);
+  @override List<Object> get props => [email, password, fullName];
+}
+
+class AuthLogoutRequested extends AuthEvent {}
