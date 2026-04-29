8c0fe9d3413c1fcd8f79d0524323086e7c9d295f
TgkCapture <tawongakanyenda5@gmail.com>
2026-04-29 09:05:51 +0200

    feat: update dependency injection to include ChannelRepository

--- mobile/lib/core/di/injection.dart
+++ mobile/lib/core/di/injection.dart
@@ -3,15 +3,15 @@ import 'package:openair/core/services/api_service.dart';
 import 'package:openair/features/auth/bloc/auth_bloc.dart';
 import 'package:openair/features/auth/repository/auth_repository.dart';
 import 'package:openair/features/settings/bloc/settings_bloc.dart';
+import 'package:openair/features/tv/repository/channel_repository.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> configureDependencies() async {
   final apiService = await ApiService.create();
   getIt.registerSingleton<ApiService>(apiService);
-  getIt.registerLazySingleton<AuthRepository>(
-    () => AuthRepository(getIt<ApiService>()),
-  );
+  getIt.registerLazySingleton<AuthRepository>(() => AuthRepository(getIt<ApiService>()));
+  getIt.registerLazySingleton<ChannelRepository>(() => ChannelRepository(getIt<ApiService>()));
   getIt.registerFactory<AuthBloc>(() => AuthBloc(getIt<AuthRepository>()));
   getIt.registerFactory<SettingsBloc>(() => SettingsBloc());
 }
